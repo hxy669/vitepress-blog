244e07e9320a35094548b0bc6a71a06d9e566227
huxy10 <xianyang.hu@hengrui.com>
2025-06-11 10:08:30 +0800

    增加评论和统计插件

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,17 +1,27 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
-import type { Theme } from 'vitepress'
+import  { inBrowser, type Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import Layout from "./Layout.vue";
+import confetti from "./components/confetti.vue";
+import VisitorPanel from "./components/VisitorPanel.vue";
+import busuanzi from "busuanzi.pure.js";
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    return h(DefaultTheme.Layout, null, {
+    return h(Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
   enhanceApp({ app, router, siteData }) {
-    // ...
+    app.component('Confetti', confetti);
+    app.component('VisitorPanel', VisitorPanel);
+    if(inBrowser){
+      router.onAfterRouteChange = () => {
+        busuanzi.fetch();
+      };
+    }
   }
 } satisfies Theme
--- index.md
+++ index.md
@@ -25,4 +25,4 @@ features:
   - title: Feature C
     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
 ---
-
+<confetti />
